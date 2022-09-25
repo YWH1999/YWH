39a69de93529ea1940d51eb2a604894e34237175
袁文华 <wenhua.yuan@vancores.com>
2022-09-26 07:43:36 +0800

    add collect

---
+++ sao-collection/config.py
@@ -0,0 +1,5 @@
+mangodb = "mongodb://*.*.*.*:*/"
+database = "saonft"
+
+contract_base_url = "https://api.opensea.io/api/v1/asset_contract/"
+collection_base_url = "https://api.opensea.io/api/v1/collection/"
